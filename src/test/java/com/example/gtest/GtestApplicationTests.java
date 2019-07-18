c1a7ae7c0daccccfa6dec46c1c0688a91f6f5a5e
Mkv-art <95780617@qq.com>
2019-07-18 13:55:37 +0800

    测试

---
+++ src/test/java/com/example/gtest/GtestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.gtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GtestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
